1879af6f19ebcacdf845ac33e953625e9d806ae2
Ashwin <ashwinrameshp@gmail.com>
2025-08-17 12:57:29 +0530

    Adding category base entity, adding migrations and , Add Readme

---
+++ src/Commercify.Core/Models/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Commercify.Core.Models;
+
+public interface IEntity
+{
+
+}
+public abstract class BaseEntity
+{
+    public DateTime CreatedAt { get; set; }
+    public DateTime LastUpdatedAt { get; set; }
+
+}
---
+++ src/Commercify.Core/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Commercify.Core.Models;
+
+[Table("Categories")]
+public class Category : BaseEntity
+{
+    public static class MaxLengths
+    {
+        public const int Name = 100;
+        public const int Description = 500;
+
+    }
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
