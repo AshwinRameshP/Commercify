49509d86f29f5bff9875de9a5b2f1fe05df3613b
Ashwin <ashwinrameshp@gmail.com>
2025-09-02 09:28:40 +0530

    program.cs add database conext. and fix typo in appsettings

--- src/Commercify.API/Program.cs
+++ src/Commercify.API/Program.cs
@@ -1,4 +1,6 @@
 using Commercify.API.Extensions;
+using Commercify.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,10 @@ builder.Services
     .AddDatabase();
 
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+dbContext.Database.Migrate();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
