23808f16855bf24e37e7221f5f6fb59ba76f4019
Ashwin <ashwinrameshp@gmail.com>
2025-09-02 12:50:28 +0530

    updating dependency injection

---
+++ src/Commercify.API/Extensions/DependencyExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Commercify.Core.Features.Categories.Create;
+using Commercify.Core.Shared;
+using Commercify.Infrastructure.Database;
+
+namespace Commercify.API.Extensions;
+
+public static class DependencyExtensions
+{
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
+    {
+        services.AddScoped<IDbContext, AppDbContext>();
+        services.AddTransient<CreateCategoryUseCase>();
+
+        return services;
+    }
+}
--- src/Commercify.API/Program.cs
+++ src/Commercify.API/Program.cs
@@ -8,9 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services
-    .AddOpenApi()
+    .AddOpenApi()   
     .AddCustomConfiguration(builder.Configuration)
-    .AddDatabase();
+    .AddDatabase()
+    .AddDependencyInjection();
 
 var app = builder.Build();
 
